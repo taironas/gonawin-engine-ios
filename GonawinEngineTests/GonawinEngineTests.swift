522f4586a79cb0b5b7c8eae9142ee9b47083f393
Remy Jourde <remy.jourde@gmail.com>
2016-02-26 15:16:52 +0100

    Project and dependencies set up

---
+++ GonawinEngineTests/GonawinEngineTests.swift
@@ -0,0 +1,36 @@
+//
+//  GonawinEngineTests.swift
+//  GonawinEngineTests
+//
+//  Created by Remy JOURDE on 26/02/2016.
+//  Copyright © 2016 Remy Jourde. All rights reserved.
+//
+
+import XCTest
+@testable import GonawinEngine
+
+class GonawinEngineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'GonawinEngine' do
+    pod 'Moya', '~> 6.1.3'
+end
+
+target 'GonawinEngineTests' do
+    pod 'Quick', '~> 0.9.1'
+    pod 'Nimble', '~> 3.1.0'
+end
+
